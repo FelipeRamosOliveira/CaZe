cae32bacfef5ef365832c4078c5fd632a13267a5
Victoria Reis <victoria.reis@coc.ufrj.br>
2022-08-13 17:47:16 -0300

    feat:docker issue

--- src/app.py
+++ src/app.py
@@ -6,33 +6,34 @@ import pandas as pd
 import pickle
 
 warnings.filterwarnings("ignore", category=FutureWarning)
-model = pickle.load(open(f'volume/model_V1.pkl', 'rb'))
+model = pickle.load(open(f'models/model_V1.pkl', 'rb'))
 
 # API SERVICE
 app = Flask(__name__)
 
 # GET STATUS
-@app.route("/", methods=["GET"]) 
+
+
+@app.route("/", methods=["GET"])
 def status():
     return jsonify({"status": "ativo"})
 
 # GET MODEL DETAILS
+
+
 @app.route("/details", methods=["GET"])
 def details():
     return jsonify({"model details": str(model)})
 
 # POST MODEL PREDICT
-@app.route("/predict", methods=["POST"]) 
+
+
+@app.route("/predict", methods=["POST"])
 def predict():
     body = request.json
     input = pd.DataFrame(body, index=[0])
     predict = model.predict(input)[0]
     return jsonify({"house value": str(predict)})
-
-
-
-
-
 
 
 if __name__ == "__main__":
